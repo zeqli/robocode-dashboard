1d074305d2badb8100f8806c73b2fc20a40f7503
Zeqing Li <32018892+zeqli-utd@users.noreply.github.com>
2017-12-16 15:47:00 -0600

    Update UsersRepository.java

--- src/main/java/app/dao/repo/UsersRepository.java
+++ src/main/java/app/dao/repo/UsersRepository.java
@@ -1,8 +1,6 @@
 package app.dao.repo;
 
-<<<<<<< HEAD
-public interface UsersRepository {
-=======
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
@@ -11,5 +9,4 @@ import app.dao.entity.Users;
 @Service
 public interface UsersRepository extends JpaRepository<Users, Long>{
 	
->>>>>>> api commit
 }
