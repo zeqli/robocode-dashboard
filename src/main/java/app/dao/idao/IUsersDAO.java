82e60e6550e66f944aa6b1b3c6abe9e5f0465e1b
Zeqing Li <32018892+zeqli-utd@users.noreply.github.com>
2017-12-16 15:45:00 -0600

    Update IUsersDAO.java

--- src/main/java/app/dao/idao/IUsersDAO.java
+++ src/main/java/app/dao/idao/IUsersDAO.java
@@ -1,12 +1,5 @@
 package app.dao.idao;
 
-<<<<<<< HEAD
-import org.springframework.stereotype.Service;
-
-@Service
-public interface IUsersDAO {
-
-=======
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -17,5 +10,4 @@ import app.dao.entity.Users;
 public interface IUsersDAO {
 	Users getUserById(Long id);
 	List<Users> getAllUser();
->>>>>>> api commit
 }
