04fb98e9933c6ee1279841bb04c882f1637782ed
Zeqing Li <32018892+zeqli-utd@users.noreply.github.com>
2017-12-16 15:46:05 -0600

    Update UserGroupRepository.java

--- src/main/java/app/dao/repo/UserGroupRepository.java
+++ src/main/java/app/dao/repo/UserGroupRepository.java
@@ -1,8 +1,5 @@
 package app.dao.repo;
 
-<<<<<<< HEAD
-public interface UserGroupRepository {
-=======
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +7,5 @@ import app.dao.entity.UserGroup;
 
 @Service
 public interface UserGroupRepository extends JpaRepository<UserGroup, Long> {
->>>>>>> api commit
+
 }
